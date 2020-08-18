56476f53ef68373eaed6c5f613217b4666da422a
penpen09 <ar-el.mbl.10@outlook.jp>
2020-08-18 16:23:10 +0900

    add 他人の投稿編集及削除制限

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -14,6 +14,9 @@ class PicturesController < ApplicationController
   end
 
   def edit
+    unless current_user.id == @picture.user_id
+    redirect_to pictures_path, notice: '他人のページは見れません'
+  end
   end
 
   def confirm
@@ -23,13 +26,17 @@ class PicturesController < ApplicationController
 
   def create
     @picture = current_user.pictures.build(picture_params)
-    respond_to do |format|
-      if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
-        # format.json { render :show, status: :created, location: @picture }
-      else
-        format.html { render :new }
-        # format.json { render json: @picture.errors, status: :unprocessable_entity }
+    if params[:back]
+      render :new
+    else
+      respond_to do |format|
+        if @picture.save
+          format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+          # format.json { render :show, status: :created, location: @picture }
+        else
+          format.html { render :new }
+          # format.json { render json: @picture.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -49,10 +56,14 @@ class PicturesController < ApplicationController
   end
 
   def destroy
-    @picture.destroy
-    respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
-      # format.json { head :no_content }
+    unless current_user.id == @picture.user_id
+      redirect_to pictures_path, notice: '他人のページは削除できません'
+    else
+      @picture.destroy
+      respond_to do |format|
+        format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+        # format.json { head :no_content }
+      end
     end
   end
 
